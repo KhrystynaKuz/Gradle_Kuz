ca246ff66a0389832c3117156caeea32b3b1829a
Khrystyna Kuz <christinakuz12@gmail.com>
2026-06-03 11:06:02 +0300

    test files

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,17 +1,22 @@
 package org.example;
 
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
-    static void main() {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        IO.println(String.format("Hello and welcome!"));
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            IO.println("i = " + i);
-        }
+    public static void main(String[] args) {
+        System.out.println("Starting test...");
+
+        // students
+        Student s1 = new Student("Khrystyna", "Kuz", 20);
+        Student s2 = new Student("Lilia", "Kotyk", 21);
+
+        // student group
+        Group group = new Group("KN-301");
+
+        group.addStudent(s1);
+        group.addStudent(s2);
+
+        // print
+        System.out.println("Group: " + group.getGroupName());
+        System.out.println("Total students in group: " + group.getStudents().size());
     }
 }
